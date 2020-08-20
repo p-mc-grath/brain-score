0b1edc4a8da00007ea1be4c50e6dc2fffbe684bb
Martin Schrimpf <m4rtinsch@gmail.com>
2020-08-20 16:55:19 -0400

    characterize raw behaviors into task values; subsample proper stimulus_set

--- brainscore/benchmarks/perturbation_prototypes.py
+++ brainscore/benchmarks/perturbation_prototypes.py
@@ -12,12 +12,11 @@ from brainscore.benchmarks import BenchmarkBase
 from brainscore.metrics import Score
 from brainscore.metrics.behavior_differences import BehaviorDifferences
 from brainscore.model_interface import BrainModel
+import scipy.io
 
 
 class Rajalingham2019(BenchmarkBase):
     def __init__(self):
-        self._stimulus_set = brainscore.get_stimulus_set('dicarlo.hvm')
-        self._stimulus_set['image_label'] = self._stimulus_set['truth'] = self._stimulus_set['category_name']
         self._target_assembly = self._load_assembly()
         self._similarity_metric = BehaviorDifferences()
         super(Rajalingham2019, self).__init__(
@@ -27,49 +26,69 @@ class Rajalingham2019(BenchmarkBase):
             paper_link='https://www.sciencedirect.com/science/article/pii/S0896627319301102')
 
     def __call__(self, candidate: BrainModel):
-        stimulus_set = self._stimulus_set
-        training_stimuli = stimulus_set  # TODO: is training the entire stimulus set?
+        # approach:
+        # 1. have model inactivate many different sites in IT
+        # 2. benchmark metric searches over those sites to find the ones that maximally correspond to 7/8 tasks
+        # 3. test generalization to 8th task (measuring distance), cross-validate
+
+        stimulus_set = self._target_assembly.stimulus_set
+        # Training
+        # TODO: Both animals were previously trained on other images of other objects, and were proficient in
+        #  discriminating among over 35 arbitrarily sampled basic-level object categories
+        training_stimuli = stimulus_set  # TODO: what were monkeys trained on? all of hvm?
         # stimulus_set = repeat_trials(number_of_trials=10) TODO
 
-        # run silencing sessions
+        # "[...] inactivation sessions were interleaved over days with control behavioral sessions.
+        # Thus, each inactivation experiment consisted of three behavioral sessions:
+        # the baseline or pre-control session (1 day prior to injection),
+        # the inactivation session,
+        # and the recovery or post-control session (2 days after injection)"
+        # --> we here front-load one control session and then run many inactivation sessions
+
+        # control
+        candidate.perturb(perturbation=None, target='IT')  # reset
+        candidate.start_task(task=BrainModel.Task.probabilities, fitting_stimuli=training_stimuli)
+        control_behavior = candidate.look_at(stimulus_set)
+        control_behavior = control_behavior.expand_dims('silenced')
+        control_behavior['silenced'] = [False]
+
+        # silencing sessions
         random_state = RandomState(0)
-        behaviors = []
-        for run in range(10):  # TODO: how many runs?
-            # "[...] inactivation sessions were interleaved over days with control behavioral sessions.
-            # Thus, each inactivation experiment consisted of three behavioral sessions:
-            # the baseline or pre-control session (1 day prior to injection),
-            # the inactivation session,
-            # and the recovery or post-control session (2 days after injection)"
-            for day in ['pre-control', 'inactivation', 'post-control']:
-                if day == 'inactivation':
-                    # "We varied the location of microinjections to randomly sample the ventral surface of IT
-                    # (from approximately + 8mm AP to approx + 20mm AP)."
-                    # TODO: is this specified via xyz? If yes, what are yz?
-                    injection_location = random_state.uniform(low=[8, 8], high=[20, 20])  # TODO: uniform or gaussian?
-                    candidate.perturb(perturbation=BrainModel.Perturbation.muscimol,
-                                      target='IT', perturbation_parameters={
-                            # "Each inactivation session began with a single focal microinjection of 1ml of muscimol
-                            # (5mg/mL, Sigma Aldrich) at a slow rate (100nl/min) via a 30-gauge stainless-steel cannula at
-                            # the targeted site in ventral IT."
-                            'amount_microliter': 1,
-                            'location': injection_location,
-                        })
-                else:  # non-inactivation day
-                    candidate.perturb(perturbation=None, target='IT')  # reset
-                candidate.start_task(task=BrainModel.Task.probabilities, fitting_stimuli=training_stimuli)
-                behavior = candidate.look_at(stimulus_set)  # TODO the whole stimulus_set each session?
-                # TODO: are the runs averaged somehow or just treated as different trials?
-                # behavior = behavior.expand_dims('run').expand_dims('day')
-                # behavior['run'], behavior['day'] = [run], [day]
-                behavior = behavior.expand_dims('day')
-                behavior['run'] = ('presentation', [run] * len(behavior['presentation']))
-                behavior['day'] = [day]
-                behaviors.append(behavior)
+        behaviors = [control_behavior]
+        for site in range(10):
+            # "We varied the location of microinjections to randomly sample the ventral surface of IT
+            # (from approximately + 8mm AP to approx + 20mm AP)."
+            injection_location = random_state.uniform(low=[8, 8], high=[20, 20])  # TODO: uniform or gaussian?
+            candidate.perturb(perturbation=BrainModel.Perturbation.muscimol,
+                              target='IT', perturbation_parameters={
+                    # "Each inactivation session began with a single focal microinjection of 1ml of muscimol
+                    # (5mg/mL, Sigma Aldrich) at a slow rate (100nl/min) via a 30-gauge stainless-steel cannula at
+                    # the targeted site in ventral IT."
+                    'amount_microliter': 1,
+                    'location': injection_location,
+                })
+            candidate.start_task(task=BrainModel.Task.probabilities, fitting_stimuli=training_stimuli)
+            behavior = candidate.look_at(stimulus_set)  # TODO the whole stimulus_set each session?
+            behavior = behavior.expand_dims('silenced').expand_dims('site')
+            behavior['silenced'] = [True]
+            behavior['site_iteration'] = 'site', [site]
+            behavior['site_x'] = 'site', [injection_location[0]]
+            behavior['site_y'] = 'site', [injection_location[1]]
+            behaviors.append(behavior)
         behaviors = merge_data_arrays(behaviors)
+
+        # TODO:
+        # Our choice of five objects resulted in ten possible pairwise object discrimination subtasks
+        # (see Figure 1A for complete list). To accumulate enough trials to precisely measure performance
+        # for each subtask within a single behavioral session (i.e., a single experimental day),
+        # we sub-selected six of these ten subtasks for most experiments
+        # TODO: how to figure out which of the 6 tasks in the data is which?
 
         # score
         # behaviors = behaviors.unstack('presentation').stack(presentation=['image_id', 'run'])
-        score = self._similarity_metric(behaviors, self._target_assembly)
+        target_assembly = self._target_assembly.sel(split=0)
+        score = self._similarity_metric(behaviors, target_assembly)
+        score = ceil(score, self.ceiling)
         return score
 
     def _load_assembly(self):
@@ -80,13 +99,31 @@ class Rajalingham2019(BenchmarkBase):
         tasks, sites = 6, 11
         rearrange = functools.partial(self._rearrange_sites_tasks, tasks_per_site=tasks, number_of_sites=sites)
         exp['d0'], exp['d1'] = rearrange(exp['d0']), rearrange(exp['d1'])
-        return DataAssembly([exp['d0'], exp['d1']], coords={
+        assembly = DataAssembly([exp['d0'], exp['d1']], coords={
             'silenced': [False, True],
             'split': np.arange(exp['d0'].shape[1]),
             'bootstrap': np.arange(exp['d0'].shape[0]),
-            'task': np.arange(tasks),
+            'task_number': ('task', np.arange(tasks)),
+            # TODO: actually assign these.
+            #  paper tasks are elephant-v-bear, dog-v-bear, dog-v-elephant, plane-v-dog, chair-v-dog, chair-v-plane
+            'task_left': ('task', ['elephant', 'dog', 'dog', 'airplane3', 'chair0', 'chair0']),
+            'task_right': ('task', ['bear', 'bear', 'elephant', 'dog', 'dog', 'airplane3']),
             'site': np.arange(sites)},
-                            dims=['silenced', 'bootstrap', 'split', 'task', 'site'])
+                                dims=['silenced', 'bootstrap', 'split', 'task', 'site'])
+        # additional tasks for experiment 2 only are plane-v-bear, plane-v-elephant, chair-v-bear, chair-v-elephant
+        assembly = assembly.mean('bootstrap')
+
+        # TODO: load stimulus_set subsampled from hvm
+        stimulus_set_ids = scipy.io.loadmat(directory / 'dat/metaparams.mat')
+        stimulus_set_ids = stimulus_set_ids['id']
+        stimulus_set_ids = [i for i in stimulus_set_ids if len(set(i)) > 1]  # filter empty ids
+        stimulus_set = brainscore.get_stimulus_set('dicarlo.hvm')
+        stimulus_set = stimulus_set[stimulus_set['image_id'].isin(stimulus_set_ids)]
+        stimulus_set['image_label'] = stimulus_set['truth'] = stimulus_set['object_name']  # 10 labels at this point
+        stimulus_set.identifier = 'dicarlo.hvm_10'
+        assembly.attrs['stimulus_set'] = stimulus_set
+        assembly.attrs['stimulus_set_identifier'] = stimulus_set.identifier
+        return assembly
 
     def _rearrange_sites_tasks(self, data, tasks_per_site, number_of_sites):
         assert data.shape[-1] == tasks_per_site * number_of_sites
--- brainscore/metrics/behavior_differences.py
+++ brainscore/metrics/behavior_differences.py
@@ -1,29 +1,106 @@
+import itertools
+
+import numpy as np
+from tqdm import tqdm
+
+from brainio_base.assemblies import merge_data_arrays, walk_coords, array_is_element
 from brainscore.metrics import Metric
 from brainscore.metrics.image_level_behavior import I2n
+
+from brainscore.metrics.transformations import TestOnlyCrossValidationSingle, CrossValidation
 
 
 class BehaviorDifferences(Metric):
     def __init__(self):
         self.i2n = I2n()
+        self.site_split = TestOnlyCrossValidationSingle()
+        self.task_split = CrossValidation()
 
     def __call__(self, assembly1, assembly2):
-        # split into control and inactivation days, ignoring postcontrol
-        assembly1 = assembly1.groupby('image_id').mean('presentation')
-        control1, inactivation1 = assembly1.sel(day='pre-control').squeeze(), assembly1.sel(day='inactivation').squeeze()
-        # control2, inactivation2 = assembly2.sel(day='pre-control'), assembly2.sel(day='inactivation')
-        # compute aggregate metrics
-        behavior_pre1, behavior_inactivation1 = self.i2n.dprimes(control1), self.i2n.dprimes(inactivation1)
-        # behavior_pre2, behavior_inactivation2 = self.i2n.dprimes(control2), self.i2n.dprimes(inactivation2)
-        behavioral_differences1 = self._compute_differences(behavior_pre1, behavior_inactivation1)
-        # behavioral_differences2 = self._compute_differences(behavior_pre2, behavior_inactivation2)
+        """
+        :param assembly1: a raw assembly in the format of `silenced: 2, presentation: p, choice: c, site: n`
+        :param assembly2: a processed assembly in the format of `silenced :2, task: c * (c-1), site: m`
+        :return: a Score
+        """
+
+        # process assembly1
+        assembly1_characterized = self.characterize(assembly1)
+        assembly1_tasks = self.subselect_tasks(assembly1, assembly2)
+        assembly1_tasks = [assembly1_characterized.sel(task_left=task_left, task_right=task_right).values for
+                           task_left, task_right in zip(assembly2['task_left'].values, assembly2['task_right'].values)]
+        assembly1_differences = self.compute_differences(assembly1_tasks)
+        assembly2_differences = self.compute_differences(assembly2)
 
         # compare
-        score = self._correlate(behavioral_differences1, behavioral_differences2)
+        site_scores = self.site_split(assembly2_differences,
+                                      apply=lambda site_assembly: self.apply_site(assembly1_differences, site_assembly))
+        score = site_scores.mean('site')
         return score
 
-    def _split_days(self, behaviors):
-        non_silenced = behaviors[{'day': [day != 'inactivation' for day in behaviors['day'].values]}]
-        silenced = behaviors.sel(day='inactivation')
-        return ...
-    def _compute_differences(self, behaviors):
-        return ...
+    def characterize(self, assembly):
+        """ compute per-task performance from `presentation x choice` assembly """
+        # xarray can't do multi-dimensional grouping, do things manually
+        o2s = []
+        adjacent_values = assembly['silenced'].values, assembly['site'].values  # TODO: this takes 4.5 min, way too long
+        for silenced, site in tqdm(itertools.product(*adjacent_values), desc='characterize',
+                                   total=np.prod([len(values) for values in adjacent_values])):
+            current_assembly = assembly.sel(silenced=silenced, site=site)
+            source_response_matrix = self.i2n.target_distractor_scores(current_assembly)
+            i2 = self.i2n.dprimes(source_response_matrix)
+            o2 = i2.groupby('truth').mean('presentation')
+            o2 = o2.rename({'truth': 'task_left', 'choice': 'task_right'})
+            o2 = o2.expand_dims('silenced').expand_dims('site')
+            o2['silenced'] = [silenced]
+            for (coord, _, _), value in zip(walk_coords(assembly['site']), site):
+                o2[coord] = 'site', [value]
+            o2s.append(o2)
+        o2s = merge_data_arrays(o2s)  # this only takes ~1s, ok
+        # a = assembly.sel(silenced=False, site=0).squeeze('site')  # TODO: unfold
+        # source_response_matrix = self.i2n.target_distractor_scores(a)
+        # i2 = self.i2n.dprimes(source_response_matrix)
+        # o2 = i2.groupby('truth').mean('presentation')
+        # o2 = o2.rename({'truth': 'task_left', 'choice': 'task_right'})
+        return o2s
+
+    def subselect_tasks(self, assembly, reference_assembly):
+        tasks_left, tasks_right = reference_assembly['task_left'].values, reference_assembly['task_right'].values
+        task_values = [assembly.sel(task_left=task_left, task_right=task_right).values
+                       for task_left, task_right in zip(tasks_left, tasks_right)]
+        task_values = type(assembly)(task_values, coords=
+        {**{
+            'task_number': ('task', reference_assembly['task_number'].values),
+            'task_left': ('task', tasks_left),
+            'task_right': ('task', tasks_right),
+        }, **{coord: (dims, values) for coord, dims, values in walk_coords(assembly)
+              if not any(array_is_element(dims, dim) for dim in ['task_left', 'task_right'])}
+         }, dims=['task'] + [dim for dim in assembly.dims if
+                             dim not in ['task_left', 'task_right']])
+        return task_values
+
+    def apply_site(self, source_assembly, site_target_assembly):
+        task_scores = self.task_split(source_assembly, site_target_assembly, apply=self.apply_task)
+        return task_scores
+
+    def apply_task(self, source_train, target_train, source_test, target_test):
+        """
+        finds the best-matching site in the source train assembly to predict the task effects in the test target.
+        :param source_train: source assembly for mapping with t tasks and n sites
+        :param target_train: target assembly for mapping with t tasks
+        :param source_test: source assembly for testing with 1 task and n sites
+        :param target_test: target assembly for testing with 1 task
+        :return: a Score
+        """
+        # map: find site in assembly1 that best matches mapping tasks
+        correlations = correlate(source_train, target_train)
+        best_site = correlations.argmax('site')
+        # test: predictivity of held-out task
+        source_test = source_test.sel(site=best_site)
+        score = target_test - source_test
+        return score
+
+    def compute_differences(self, behaviors):
+        """
+        :param behaviors: an assembly with a dimension `silenced` and values `[True, False]`
+        :return: the difference between these two conditions
+        """
+        return behaviors.sel(silenced=False) - behaviors.sel(silenced=True)
