dbf1a0f6cfcd23862f2f3b1d42295979d5197122
Martin Schrimpf <m4rtinsch@gmail.com>
2020-08-20 22:27:09 -0400

    implement mapping procedure

--- brainscore/metrics/behavior_differences.py
+++ brainscore/metrics/behavior_differences.py
@@ -1,6 +1,7 @@
 import itertools
 
 import numpy as np
+from scipy.stats import pearsonr
 from tqdm import tqdm
 
 from brainio_base.assemblies import merge_data_arrays, walk_coords, array_is_element
@@ -8,59 +9,60 @@ from brainscore.metrics import Metric
 from brainscore.metrics.image_level_behavior import I2n
 
 from brainscore.metrics.transformations import TestOnlyCrossValidationSingle, CrossValidation
+from result_caching import store
 
 
 class BehaviorDifferences(Metric):
     def __init__(self):
         self.i2n = I2n()
-        self.site_split = TestOnlyCrossValidationSingle()
-        self.task_split = CrossValidation()
 
     def __call__(self, assembly1, assembly2):
         """
-        :param assembly1: a raw assembly in the format of `silenced: 2, presentation: p, choice: c, site: n`
+        :param assembly1: a tuple with the first element representing the control behavior in the format of
+            `presentation: p, choice: c` and the second element representing inactivations behaviors in
+            `presentation: p, choice: c, site: n`
         :param assembly2: a processed assembly in the format of `silenced :2, task: c * (c-1), site: m`
         :return: a Score
         """
 
         # process assembly1
         assembly1_characterized = self.characterize(assembly1)
-        assembly1_tasks = self.subselect_tasks(assembly1, assembly2)
-        assembly1_tasks = [assembly1_characterized.sel(task_left=task_left, task_right=task_right).values for
-                           task_left, task_right in zip(assembly2['task_left'].values, assembly2['task_right'].values)]
+        assembly1_tasks = self.subselect_tasks(assembly1_characterized, assembly2)
         assembly1_differences = self.compute_differences(assembly1_tasks)
         assembly2_differences = self.compute_differences(assembly2)
 
         # compare
-        site_scores = self.site_split(assembly2_differences,
-                                      apply=lambda site_assembly: self.apply_site(assembly1_differences, site_assembly))
+        site_split = TestOnlyCrossValidationSingle(  # instantiate on-the-fly to control the kfolds for 1 test site each
+            split_coord='site', stratification_coord=None, kfold=True, splits=len(assembly2['site']))
+        site_scores = site_split(assembly2_differences,
+                                 apply=lambda site_assembly: self.apply_site(assembly1_differences, site_assembly))
         score = site_scores.mean('site')
         return score
 
+    @store(identifier_ignore=['assembly'])
     def characterize(self, assembly):
         """ compute per-task performance from `presentation x choice` assembly """
         # xarray can't do multi-dimensional grouping, do things manually
         o2s = []
-        adjacent_values = assembly['silenced'].values, assembly['site'].values  # TODO: this takes 4.5 min, way too long
+        adjacent_values = assembly['silenced'].values, assembly['site'].values  # TODO: this takes 2min (4.5 in debug)
         for silenced, site in tqdm(itertools.product(*adjacent_values), desc='characterize',
                                    total=np.prod([len(values) for values in adjacent_values])):
             current_assembly = assembly.sel(silenced=silenced, site=site)
-            source_response_matrix = self.i2n.target_distractor_scores(current_assembly)
-            i2 = self.i2n.dprimes(source_response_matrix)
-            o2 = i2.groupby('truth').mean('presentation')
-            o2 = o2.rename({'truth': 'task_left', 'choice': 'task_right'})
+            o2 = self.o2(current_assembly)
             o2 = o2.expand_dims('silenced').expand_dims('site')
             o2['silenced'] = [silenced]
             for (coord, _, _), value in zip(walk_coords(assembly['site']), site):
                 o2[coord] = 'site', [value]
             o2s.append(o2)
         o2s = merge_data_arrays(o2s)  # this only takes ~1s, ok
-        # a = assembly.sel(silenced=False, site=0).squeeze('site')  # TODO: unfold
-        # source_response_matrix = self.i2n.target_distractor_scores(a)
-        # i2 = self.i2n.dprimes(source_response_matrix)
-        # o2 = i2.groupby('truth').mean('presentation')
-        # o2 = o2.rename({'truth': 'task_left', 'choice': 'task_right'})
         return o2s
+
+    def o2(self, assembly):
+        response_matrix = self.i2n.target_distractor_scores(assembly)
+        i2 = self.i2n.dprimes(response_matrix)
+        o2 = i2.groupby('truth').mean('presentation')
+        o2 = o2.rename({'truth': 'task_left', 'choice': 'task_right'})
+        return o2
 
     def subselect_tasks(self, assembly, reference_assembly):
         tasks_left, tasks_right = reference_assembly['task_left'].values, reference_assembly['task_right'].values
@@ -78,7 +80,15 @@ class BehaviorDifferences(Metric):
         return task_values
 
     def apply_site(self, source_assembly, site_target_assembly):
-        task_scores = self.task_split(source_assembly, site_target_assembly, apply=self.apply_task)
+        site_target_assembly = site_target_assembly.squeeze('site')
+        np.testing.assert_array_equal(source_assembly.sortby('task_number')['task_left'].values,
+                                      site_target_assembly.sortby('task_number')['task_left'].values)
+        np.testing.assert_array_equal(source_assembly.sortby('task_number')['task_right'].values,
+                                      site_target_assembly.sortby('task_number')['task_right'].values)
+        task_split = CrossValidation(split_coord='task_number', stratification_coord=None,
+                                     kfold=True, splits=len(site_target_assembly['task']))
+        task_scores = task_split(source_assembly, site_target_assembly, apply=self.apply_task)
+        task_scores = task_scores.raw
         return task_scores
 
     def apply_task(self, source_train, target_train, source_test, target_test):
@@ -91,12 +101,21 @@ class BehaviorDifferences(Metric):
         :return: a Score
         """
         # map: find site in assembly1 that best matches mapping tasks
-        correlations = correlate(source_train, target_train)
-        best_site = correlations.argmax('site')
+        correlations = {}
+        for site in source_train['site'].values:
+            source_site = source_train.sel(site=site)
+            np.testing.assert_array_equal(source_site['task'].values, target_train['task'].values)
+            correlation = pearsonr(source_site.values, target_train.values)
+            correlations[site] = correlation
+        best_site = [site for site, correlation in correlations.items() if correlation == max(correlations.values())]
         # test: predictivity of held-out task
-        source_test = source_test.sel(site=best_site)
-        score = target_test - source_test
-        return score
+        source_test = source_test.sel(site=best_site).squeeze('site')
+        np.testing.assert_array_equal(source_test['task'].values, target_test['task'].values)
+        pair = type(target_test)([source_test.values[0], target_test.values[0]],
+                                 coords={  # 'task': source_test['task'].values,
+                                     'type': ['source', 'target']},
+                                 dims=['type'])  # , 'task'
+        return pair
 
     def compute_differences(self, behaviors):
         """
