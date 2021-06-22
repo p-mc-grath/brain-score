2dc18aeb5cbac9f4c7a6ef4685d56cfe6229535f
pmcgrath249 <pmcgrath249@gmail.com>
2021-06-22 12:00:27 +0200

    init after messup

--- brainscore/benchmarks/majajhong2015.py
+++ brainscore/benchmarks/majajhong2015.py
@@ -1,10 +1,16 @@
 import brainscore
+from brainscore.model_interface import BrainModel
+from brainscore.benchmarks import BenchmarkBase
 from brainscore.benchmarks._neural_common import NeuralBenchmark, average_repetition
 from brainscore.metrics.ceiling import InternalConsistency, RDMConsistency
 from brainscore.metrics.rdm import RDMCrossValidated
 from brainscore.metrics.regression import CrossRegressedCorrelation, mask_regression, ScaledCrossRegressedCorrelation, \
     pls_regression, pearsonr_correlation
+from brainscore.metrics.spatial_correlation import SpatialCorrelationSimilarity
+from brainscore.metrics.inter_individual_stats_ceiling import InterIndividualStatisticsCeiling
 from brainscore.utils import LazyLoad
+import numpy as np
+from scipy.spatial.distance import squareform, pdist
 
 VISUAL_DEGREES = 8
 NUMBER_OF_TRIALS = 50
@@ -91,3 +97,207 @@ def load_assembly(average_repetitions, region, access='private'):
     if average_repetitions:
         assembly = average_repetition(assembly)
     return assembly
+
+
+class DicarloMajajHong2015ITSpatialCorrelation(BenchmarkBase):
+
+    def __init__(self):
+        '''
+        This benchmark compares the distribution of pairwise response correlation as a function of distance between the
+            MajajHong2015 assembly and a candidate BrainModel
+        Plots of this can be found in Figure 2 of the corresponding publication (see BIBTEX)
+        '''
+        super().__init__(identifier='dicarlo.MajajHong2015.IT-spatial_correlation',
+                         ceiling_func=lambda: InterIndividualStatisticsCeiling(
+                             SpatialCorrelationSimilarity(similarity_function=self.inv_ks_similarity, bin_size_mm=.1))(
+                             LazyLoad(self.compute_global_tissue_statistic_target)),  # .1 mm is an arbitrary choice
+                         version=0.1,
+                         parent='IT',
+                         bibtex=BIBTEX)
+
+        assembly = brainscore.get_assembly('dicarlo.MajajHong2015').sel(region='IT')
+        assembly = self.make_static(assembly)
+        assembly = self.tissue_update(assembly)
+
+        self._neuroid_reliability = InternalConsistency()(assembly.transpose('presentation', 'neuroid'))
+        self._stimulus_set = assembly.stimulus_set
+        self._target_assembly = average_repetition(assembly)
+        self._score = SpatialCorrelationSimilarity(similarity_function=self.inv_ks_similarity,
+                                                   bin_size_mm=.1)  # .1 mm is an arbitrary choice
+
+        self.bootstrap_samples = 100_000
+        self.num_sample_arrs = 10  # number of simulated Utah arrays sampled from candidate model tissue
+        self._array_size_mm = (np.ptp(self._target_assembly.neuroid.tissue_x.data),  # physical size of Utah array in mm
+                               np.ptp(self._target_assembly.neuroid.tissue_y.data))
+
+    def __call__(self, candidate: BrainModel):
+        '''
+        This computes the statistics, i.e. the pairwise response correlation of candidate and target, respectively and
+        computes a score based on the ks similarity of the two resulting distributions
+        :param candidate: BrainModel
+        :return: Score, i.e. average inverted ks similarity, for the pairwise response correlation compared to the MajajHong Assembly
+        '''
+        candidate.start_recording(recording_target='IT', time_bins=[(70, 170)])
+        candidate_assembly = candidate.look_at(self._stimulus_set)
+        candidate_assembly = self.make_static(candidate_assembly)
+        candidate_statistic = self.sample_global_tissue_statistic(candidate_assembly)
+
+        self._target_statistic = self.compute_global_tissue_statistic_target()
+
+        score = self._score(np.hstack(self._target_statistic), candidate_statistic)
+        score.attrs['target_statistic'] = self._target_statistic
+        score.attrs['candidate_statistic'] = candidate_statistic
+
+        return score
+
+    def sample_global_tissue_statistic(self, candidate_assembly):
+        '''
+        Simulates placement of multiple arrays in tissue and computes repsonse correlation as a function of distance on
+        each of them
+        :param candidate_assembly: NeuroidAssembly
+        :return: list of lists, [0] pairwise reponse correlation, [1] corresponding distances
+        '''
+        candidate_statistic = []
+        for window in self.sample_array_locations(candidate_assembly.neuroid, self.num_sample_arrs,
+                                                  self._array_size_mm):
+            array_statistic = self.sample_response_corr_vs_dist(candidate_assembly[window],
+                                                                int(self.bootstrap_samples / self.num_sample_arrs))
+            candidate_statistic.append(array_statistic)
+
+        return np.hstack(candidate_statistic)
+
+    def compute_global_tissue_statistic_target(self):
+        '''
+        :return: list of [pairwise distances, pairwise response correlations], each item is another animal/array
+            combination from the MajajHong Assembly
+        '''
+        target_statistic = []
+        for animal in sorted(list(set(self._target_assembly.neuroid.animal.data))):
+            for arr in sorted(list(set(self._target_assembly.neuroid.arr.data))):
+                sub_assembly = self._target_assembly.sel(animal=animal, arr=arr)
+                bootstrap_samples_sub_assembly = int(self.bootstrap_samples * (sub_assembly.neuroid.size /
+                                                                               self._target_assembly.neuroid.size))
+                sub_assembly_statistic = self.sample_response_corr_vs_dist(sub_assembly, bootstrap_samples_sub_assembly,
+                                                                           self._neuroid_reliability)
+                target_statistic.append(sub_assembly_statistic)
+
+        return target_statistic
+
+    @classmethod
+    def sample_response_corr_vs_dist(cls, assembly, num_samples, neuroid_reliability=None, seed=0):
+        '''
+        1. Samples random pairs from the assembly
+        2. Computes distances for all pairs
+        3. Computes the response correlation between items of each pair
+        (4. Ceils the response correlations by ceiling each neuroid | if neuroid_reliability not None)
+        :param assembly: NeuroidAssembly without stimulus repetitions
+        :param num_samples: how many random pair you want to be sampled out of the data
+        :param neuroid_reliability: if not None: expecting Score object containing reliability estimates of all neuroids
+        :param seed: random seed
+        :return: [distance, pairwise_correlation_of_neuroids], pairwise correlations can be ceiled
+        '''
+        rng = np.random.default_rng(seed=seed)
+        neuroid_pairs = rng.integers(0, assembly.shape[0], (2, num_samples))
+
+        pairwise_distances_all = cls.pairwise_distances(assembly)
+        pairwise_distance_samples = pairwise_distances_all[(*neuroid_pairs,)]
+
+        response_samples = assembly.data[neuroid_pairs]
+        response_correlation_samples = np.nan_to_num(cls.corrcoef_rowwise(*response_samples))
+
+        if neuroid_reliability is not None:
+            pairwise_neuroid_reliability_all = cls.create_pairwise_neuroid_reliability_mat(neuroid_reliability)
+            pairwise_neuroid_reliability_samples = pairwise_neuroid_reliability_all[(*neuroid_pairs,)]
+            response_correlation_samples = response_correlation_samples / pairwise_neuroid_reliability_samples
+
+        return np.vstack((pairwise_distance_samples, response_correlation_samples))
+
+    @staticmethod
+    def corrcoef_rowwise(a, b):
+        # https://stackoverflow.com/questions/41700840/correlation-of-2-time-dependent-multidimensional-signals-signal-vectors
+        a_ma = a - a.mean(1)[:, None]
+        b_mb = b - b.mean(1)[:, None]
+        ssa = np.einsum('ij,ij->i', a_ma, a_ma)  # var A
+        ssb = np.einsum('ij,ij->i', b_mb, b_mb)  # var B
+        return np.einsum('ij,ij->i', a_ma, b_mb) / np.sqrt(ssa * ssb)  # cov/sqrt(varA*varB)
+
+    @staticmethod
+    def pairwise_distances(assembly):
+        '''
+        Convenience function creating a simple lookup table for pairwise distances
+        :param assembly: NeuroidAssembly
+        :return: square matrix where each entry is the distance between the neuroids at the corresponding indices
+        '''
+        locations = np.stack([assembly.neuroid.tissue_x.data, assembly.neuroid.tissue_y.data]).T
+
+        return squareform(pdist(locations, metric='euclidean'))
+
+    @staticmethod
+    def create_pairwise_neuroid_reliability_mat(neuroid_reliability):
+        '''
+        Convenience function creating a simple lookup table for combined reliabilities of neuroid pairs
+        :param neuroid_reliability: expects Score object where neuroid_reliability.raw holds [cross validation subset,
+            reliability per neuroid]
+        :return: square matrix where each entry_ij = sqrt(reliability_i * reliability_j)
+        '''
+        reliability_per_neuroid = np.mean(neuroid_reliability.raw.data, axis=0)
+        c_mat = np.zeros((reliability_per_neuroid.size, reliability_per_neuroid.size))
+        for i, ci in enumerate(reliability_per_neuroid):
+            for j, cj in enumerate(reliability_per_neuroid):
+                c_mat[i, j] = np.sqrt(ci * cj)
+
+        return c_mat
+
+    @staticmethod
+    def sample_array_locations(neuroid, num_sample_arrs, array_size_mm, seed=0):
+        '''
+        Generator: Sample Utah array-like portions from artificial model tissue and generate masks
+        :param neuroid: NeuroidAssembly.neuroid
+        :param num_sample_arrs: number of simulated Utah arrays you want to sample
+        :param array_size_mm: Tuple, physical size of the arrays you want to sample in mm
+        :param seed: random seed
+        :return: list of masks in neuroid dimension of assembly, usage: assembly[mask] -> neuroids within one array
+        '''
+        bound_max_x, bound_max_y = np.max([neuroid.tissue_x.data, neuroid.tissue_y.data], axis=1) - array_size_mm
+        rng = np.random.default_rng(seed=seed)
+        lower_corner = np.column_stack((rng.choice(neuroid.tissue_x.data[neuroid.tissue_x.data <= bound_max_x],
+                                                   size=num_sample_arrs),
+                                        rng.choice(neuroid.tissue_y.data[neuroid.tissue_y.data <= bound_max_y],
+                                                   size=num_sample_arrs)))
+        upper_corner = lower_corner + array_size_mm
+
+        # create index masks of neuroids within sample windows
+        for i in range(num_sample_arrs):
+            yield np.logical_and.reduce([neuroid.tissue_x.data <= upper_corner[i, 0],
+                                         neuroid.tissue_x.data >= lower_corner[i, 0],
+                                         neuroid.tissue_y.data <= upper_corner[i, 1],
+                                         neuroid.tissue_y.data >= lower_corner[i, 1]])
+
+    @staticmethod
+    def tissue_update(assembly):
+        '''
+        Temporary functions: Obsolete when all saved assemblies updated such that x and y coordinates of each array electrode
+        are stored in assembly.neuroid.tissue_{x,y}
+        '''
+        if not hasattr(assembly, 'tissue_x'):
+            assembly['tissue_x'] = assembly['x']
+            assembly['tissue_y'] = assembly['y']
+
+        return assembly
+
+    @staticmethod
+    def make_static(assembly):
+        if 'time_bin' in assembly.dims:
+            assembly = assembly.squeeze('time_bin')
+        if hasattr(assembly, "time_step"):
+            assembly = assembly.squeeze("time_step")
+
+        return assembly
+
+    @staticmethod
+    def inv_ks_similarity(p, q):
+        '''
+        Inverted ks similarity -> resulting in a score within [0,1], 1 being a perfect match
+        '''
+        import scipy.stats
+        return 1 - scipy.stats.ks_2samp(p, q)[0]
---
+++ brainscore/metrics/spatial_correlation.py
@@ -0,0 +1,65 @@
+import numpy as np
+from brainscore.metrics import Score, Metric
+
+
+def _aggregate(scores):
+    '''
+    Aggregates list of values into Score object
+    :param scores: list of values assumed to be scores
+    :return: Score object | where score['center'] = mean(scores) and score['error'] = std(scores)
+    '''
+    center = np.mean(scores)
+    error = np.std(scores)
+    aggregate_score = Score([center, error], coords={'aggregation': ['center', 'error']}, dims=('aggregation',))
+    aggregate_score.attrs[Score.RAW_VALUES_KEY] = scores
+
+    return aggregate_score
+
+
+class SpatialCorrelationSimilarity(Metric):
+    '''
+    Computes the similarity of two given distributions using a given similarity_function. The similarity_function is
+    applied to each bin which in turn are created based on a given bin size and the independent variable of the
+    distributions
+    '''
+
+    def __init__(self, similarity_function, bin_size_mm):
+        '''
+        :param similarity_function: similarity_function to be applied to each bin
+        :param bin_size_mm: size per bin in mm | one fixed size, utilize Score.RAW_VALUES_KEY to change weighting
+        '''
+        self.similarity_function = similarity_function
+        self.bin_size = bin_size_mm
+
+    def __call__(self, target_statistic, candidate_statistic):
+        '''
+        :param target_statistic: list of 2 lists, [0] distances -> binning over this, [1] correlation per distance value
+        :param candidate_statistic: list of 2 lists, [0] distances -> binning over this, [1] correlation per distance value
+        '''
+        self.target_statistic = target_statistic
+        self.candidate_statistic = candidate_statistic
+        self._bin_min = np.min(self.target_statistic[0])
+        self._bin_max = np.max(self.target_statistic[0])
+
+        bin_scores = []
+        for in_bin_t, in_bin_c, enough_data in self._bin_masks():
+            if enough_data:
+                bin_scores.append(self.similarity_function(self.target_statistic[1, in_bin_t],
+                                                           self.candidate_statistic[1, in_bin_c]))
+    
+        return _aggregate(bin_scores)
+
+    def _bin_masks(self):
+        '''
+        Generator: Yields masks indexing which elements are within each bin.
+        :yield: mask(target, current_bin), mask(candidate, current_bin), enough data in the bins to do further computations
+        '''
+        for lower_bound_mm in np.linspace(self._bin_min, self._bin_max,
+                                          int(self._bin_max * (1 / self.bin_size) + 1)):
+            t = np.where(np.logical_and(self.target_statistic[0] >= lower_bound_mm,
+                                        self.target_statistic[0] < lower_bound_mm + self.bin_size))[0]
+            c = np.where(np.logical_and(self.candidate_statistic[0] >= lower_bound_mm,
+                                        self.candidate_statistic[0] < lower_bound_mm + self.bin_size))[0]
+            enough_data = t.size > 30 and c.size > 30  # random threshold
+
+            yield t, c, enough_data
