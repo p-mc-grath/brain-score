e055af9bb811892ba55daab322447b42216a58a0
Martin Schrimpf <m4rtinsch@gmail.com>
2021-07-09 17:19:40 -0400

    package Afraz2015, fig. 3C

--- brainio_packaging/afraz2015/__init__.py
+++ brainio_packaging/afraz2015/__init__.py
@@ -5,8 +5,8 @@ import pandas as pd
 from brainio_base.assemblies import DataAssembly
 
 
-def collect_assembly():
-    # fig 3A
+def collect_delta_overall_accuracy():
+    """ fig 3A """
     # data extracted with https://apps.automeris.io/wpd/ on 2021-07-09, points manually selected
     data = pd.read_csv(Path(__file__).parent / 'fig3A.csv')
     errors = data.groupby(['visual_field', 'condition']).apply(  # apply)
@@ -26,4 +26,19 @@ def collect_assembly():
                                                       for condition in assembly['condition_description']])
     assembly = assembly.stack(condition=['condition_description'])
     assembly = DataAssembly(assembly)
+    return assembly
+
+
+def collect_per_site_delta():
+    """ fig 3C """
+    # data extracted with https://apps.automeris.io/wpd/ on 2021-07-09, points manually selected
+    # Note that there is a slight discrepancy in the correlation which is -0.46 in the paper,
+    # but turns out to be -0.472 with the annotated dots
+    data = pd.read_csv(Path(__file__).parent / 'fig3C.csv')
+
+    # package into xarray
+    assembly = DataAssembly(data['delta_accuracy'], coords={
+        'monkey': ('site', data['monkey']),
+        'face_detection_index_dprime': ('site', data['face_detection_index_dprime']),
+    }, dims=['site'])
     return assembly
