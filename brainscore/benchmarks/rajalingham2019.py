26f5a7a834a3b35f5cbe004c39d1f3c883a30bd3
Martin Schrimpf <m4rtinsch@gmail.com>
2021-07-30 22:18:13 -0400

    unify assembly collection

--- brainscore/benchmarks/rajalingham2019.py
+++ brainscore/benchmarks/rajalingham2019.py
@@ -161,34 +161,35 @@ def sample_grid_points(low, high, num_x, num_y):
 
 
 class DicarloRajalingham2019SpatialDeficits(BenchmarkBase):
-
     def __init__(self):
-        super().__init__(identifier='dicarlo.Rajalingham2019.IT-spatial_deficit',
+        super().__init__(identifier='dicarlo.Rajalingham2019.IT-spatial_deficit_similarity',
                          ceiling_func=lambda: None,
                          version=0.1,
                          parent='IT',
                          bibtex=BIBTEX)
-        self._target_assembly = self._load_assembly()
+        self._target_assembly = collect_assembly()
         self._stimulus_set = self._target_assembly.stimulus_set
         self._target_statistic = LazyLoad(self.compute_response_deficit_distance_target)
-        self._score = SignificantCorrelation(x_coord='distance', ignore_nans=True)
+        self._metric = SignificantCorrelation(x_coord='distance')
 
         self.perturbation = {'type': BrainModel.Perturbation.muscimol,
                              'target': 'IT',
-                             'perturbation_parameters': {'amount_microliter': 1,
+                             'perturbation_parameters': {'amount_µl': 1,
                                                          'location': None}}
 
     def __call__(self, candidate: BrainModel):
-        candidate_assembly = self.create_dprime_assembly(candidate)
+        behaviors = self.run_experiment_on_model(candidate)
+        dprime_assembly_all = self.characterize(behaviors)
+        dprime_assembly = self.subselect_tasks(dprime_assembly_all, self._target_assembly)
+        candidate_assembly = dprime_assembly.transpose('injected', 'site', 'task')  # match target assembly shape
         candidate_statistic = self.compute_response_deficit_distance_candidate(candidate_assembly)
 
-        score = self._score(self._target_statistic, candidate_statistic)
-        score.attrs['target_statistic'] = self._target_statistic
-        score.attrs['candidate_statistic'] = candidate_statistic
-
+        score = self._metric(candidate_statistic, self._target_statistic)
+        # score.attrs['target_statistic'] = self._target_statistic
+        # score.attrs['candidate_statistic'] = candidate_statistic
         return score
 
-    def create_dprime_assembly(self, candidate: BrainModel):
+    def run_experiment_on_model(self, candidate: BrainModel):
         training_stimuli = self._stimulus_set  # TODO: true?
         candidate.start_task(task=BrainModel.Task.probabilities, fitting_stimuli=training_stimuli)
         unperturbed_behavior = self.perform_task(candidate, perturbation=None)
@@ -205,11 +206,7 @@ class DicarloRajalingham2019SpatialDeficits(BenchmarkBase):
 
         behaviors = merge_data_arrays(behaviors)
         behaviors = self.align_task_names(behaviors)
-
-        dprime_assembly_all = self.characterize(behaviors)
-        dprime_assembly = self.subselect_tasks(dprime_assembly_all, self._target_assembly)
-
-        return dprime_assembly.transpose('injected', 'site', 'task')  # match target assembly shape
+        return behaviors
 
     def perform_task(self, candidate: BrainModel, perturbation):
         if perturbation is None:
@@ -293,84 +290,6 @@ class DicarloRajalingham2019SpatialDeficits(BenchmarkBase):
             for coord, dims, values in walk_coords(behaviors)},
                                     dims=behaviors.dims)
         return behaviors
-
-    def _load_assembly(self, contra_hemisphere=True):
-        """
-        :param contra_hemisphere: whether to only select data and associated stimuli
-            where the target object was contralateral to the injection hemisphere
-        """
-        path = Path(__file__).parent / 'Rajalingham2019_data_summary.mat'
-        data = scipy.io.loadmat(path)['data_summary']
-        struct = {d[0]: v for d, v in zip(data.dtype.descr, data[0, 0])}
-        tasks = [v[0] for v in struct['O2_task_names'][:, 0]]
-        tasks_left, tasks_right = zip(*[task.split(' vs. ') for task in tasks])
-        k1 = {d[0]: v for d, v in zip(struct['k1'].dtype.descr, struct['k1'][0, 0])}
-
-        class missing_dict(dict):
-            def __missing__(self, key):
-                return key
-
-        dim_replace = missing_dict({'sub_metric': 'hemisphere', 'nboot': 'bootstrap', 'exp': 'site',
-                                    'niter': 'trial_split', 'subj': 'subject'})
-        condition_replace = {'ctrl': 'saline', 'inj': 'muscimol'}
-        dims = [dim_replace[v[0]] for v in k1['dim_labels'][0]]
-        subjects = [v[0] for v in k1['subjs'][0]]
-        conditions, subjects = zip(*[subject.split('_') for subject in subjects])
-        metrics = [v[0] for v in k1['metrics'][0]]
-        assembly = DataAssembly([k1['D0'], k1['D1']],
-                                coords={
-                                    'injected': [True, False],
-                                    'injection': (dim_replace['subj'], [condition_replace[c] for c in conditions]),
-                                    'subject_id': (dim_replace['subj'], list(subjects)),
-                                    'metric': metrics,
-                                    dim_replace['sub_metric']: ['all', 'ipsi', 'contra'],
-                                    # autofill
-                                    dim_replace['niter']: np.arange(k1['D0'].shape[3]),
-                                    dim_replace['k']: np.arange(k1['D0'].shape[4]),
-                                    dim_replace['nboot']: np.arange(k1['D0'].shape[5]),
-                                    'site_number': ('site', np.arange(k1['D0'].shape[6])),
-                                    'site_iteration': ('site', np.arange(k1['D0'].shape[6])),
-                                    'experiment': ('site', np.arange(k1['D0'].shape[6])),
-                                    'task_number': ('task', np.arange(k1['D0'].shape[7])),
-                                    'task_left': ('task', list(tasks_left)),
-                                    'task_right': ('task', list(tasks_right)),
-                                },
-                                dims=['injected'] + dims)
-        assembly['monkey'] = 'site', ['M' if site <= 9 else 'P' for site in assembly['site_number'].values]
-        assembly = assembly.squeeze('k').squeeze('trial_split')
-        if contra_hemisphere:
-            assembly = assembly.sel(hemisphere='contra')
-        assembly = assembly.sel(metric='o2_dp')
-        assembly = assembly[{'subject': [injection == 'muscimol' for injection in assembly['injection'].values]}]
-        assembly = assembly.squeeze('subject')  # squeeze single-element subject dimension since data are pooled already
-
-        # add site locations
-        path = Path(__file__).parent / 'xray_3d.mat'
-        site_locations = scipy.io.loadmat(path)['MX']
-        assembly['site_x'] = 'site', site_locations[:, 0] / 1000  # scaling micro m to mm
-        assembly['site_y'] = 'site', site_locations[:, 1] / 1000
-        assembly['site_z'] = 'site', site_locations[:, 2] / 1000
-        assembly = DataAssembly(assembly)  # reindex
-
-        # load stimulus_set subsampled from hvm
-        stimulus_set_meta = scipy.io.loadmat('/braintree/home/msch/rr_share_topo/topoDCNN/dat/metaparams.mat')
-        stimulus_set_ids = stimulus_set_meta['id']
-        stimulus_set_ids = [i for i in stimulus_set_ids if len(set(i)) > 1]  # filter empty ids
-        stimulus_set = brainscore.get_stimulus_set('dicarlo.hvm')
-        stimulus_set = stimulus_set[stimulus_set['image_id'].isin(stimulus_set_ids)]
-        stimulus_set = stimulus_set[stimulus_set['object_name'].isin(TASK_LOOKUP)]
-        stimulus_set['image_label'] = stimulus_set['truth'] = stimulus_set['object_name']  # 10 labels at this point
-        stimulus_set.identifier = 'dicarlo.hvm_10'
-        if contra_hemisphere:
-            stimulus_set = stimulus_set[(stimulus_set['rxz'] > 0) & (stimulus_set['variation'] == 6)]
-        assembly.attrs['stimulus_set'] = stimulus_set
-        assembly.attrs['stimulus_set_identifier'] = stimulus_set.identifier
-        return assembly
-
-    @staticmethod
-    def _rearrange_sites_tasks(data, tasks_per_site, number_of_sites):
-        assert data.shape[-1] == tasks_per_site * number_of_sites
-        return np.reshape(data, list(data.shape[:-1]) + [tasks_per_site, number_of_sites], order='F')
 
     @property
     def ceiling(self):
